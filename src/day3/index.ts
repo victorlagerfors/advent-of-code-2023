6ec9443bb90f78bc3e162fd9f3aa9a33295d9460
Victor Lagerfors <vlag@netlight.com>
2023-12-03 13:45:45 +0100

    day3 part 2

--- src/day3/index.ts
+++ src/day3/index.ts
@@ -1,7 +1,7 @@
 import { readFileToString } from "../../utils/file-parser";
 
-interface Coordinate {
-  currentNumber: number;
+interface Star {
+  neighbors: number[];
   x: number;
   y: number;
 }
@@ -12,7 +12,6 @@ const parseInput = () => {
     .map((str) => str.replace("\n", ""));
   const numberRegex = /\d+/g;
 
-  let numbersNeighborsArray: Coordinate[] = [];
   let characterGrid: string[][] = [];
   let currentRow = 0;
   rows.forEach((row, index) => {
@@ -28,6 +27,41 @@ const parseInput = () => {
     currentRow++;
   });
   console.log(foundNumbers.reduce((prev, curr) => prev + curr));
+
+  currentRow = 0;
+
+  let starObj = {};
+  for (let i = 0; i < characterGrid.length; i++) {
+    for (let j = 0; j < characterGrid[0].length; j++) {
+      if (characterGrid[i][j] === "*") {
+        starObj[`${i},${j}`] = [];
+      }
+    }
+  }
+
+  rows.forEach((row) => {
+    const matches = row.matchAll(numberRegex);
+    foundNumbers.push(
+      ...findNeighborsForStarMatch(starObj, characterGrid, currentRow, matches)
+    );
+    currentRow++;
+  });
+
+  console.log([
+    ...Object.values(starObj)
+      .filter((obj) => (obj as any[]).length >= 2)
+      .map((obj) => obj[0] * obj[1]),
+  ]);
+
+  console.log(
+    [
+      ...Object.values(starObj)
+        .filter((obj) => (obj as any[]).length >= 2)
+        .map((obj) => obj[0] * obj[1]),
+    ]
+      .flat()
+      .reduce((prev, curr) => (prev as number) + (curr as number), 0)
+  );
 };
 
 const findNeighborsForRegexMatch = (
@@ -40,7 +74,6 @@ const findNeighborsForRegexMatch = (
     let matchIndex = match.index;
     let currentIndex = 0;
     let foundMatch = false;
-    let valueOfFoundMatch = 0;
     for (const [testedCharacter] of match[0]) {
       for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
@@ -62,7 +95,43 @@ const findNeighborsForRegexMatch = (
       if (foundMatch) break;
     }
   }
-  console.log(foundNumbers);
+  return foundNumbers;
+};
+
+const findNeighborsForStarMatch = (
+  starGrid: object,
+  characterGrid: string[][],
+  currentRow: number,
+  matches: IterableIterator<RegExpMatchArray>
+) => {
+  const foundNumbers = [];
+  for (const match of matches) {
+    let matchIndex = match.index;
+    let currentIndex = 0;
+    let foundMatch = false;
+    for (const [testedCharacter] of match[0]) {
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          if (
+            characterGrid[currentRow + i] &&
+            characterGrid[currentRow + i][matchIndex + j] &&
+            characterGrid[currentRow + i][matchIndex + j] === "*"
+          ) {
+            if (match[0] === "126") debugger;
+            foundMatch = true;
+            starGrid[`${currentRow + i},${matchIndex + j}`].push(
+              Number(match[0])
+            );
+            break;
+          }
+        }
+        if (foundMatch) break;
+      }
+      matchIndex++;
+      currentIndex++;
+      if (foundMatch) break;
+    }
+  }
   return foundNumbers;
 };
 
